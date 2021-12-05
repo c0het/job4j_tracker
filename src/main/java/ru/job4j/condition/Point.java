8efad3425a64d3d8079c709522bd32e61feb26c0
c0het <c0het401@gmail.com>
2021-12-05 14:37:53 +0300

    Рефакторинг - Расстояние между точками.

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(that.x - this.x, 2) + pow(that.y - this.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point point = new Point(2, 5);
+        Point point1 = new Point(4, 6);
+        double dist = point.distance(point1);
+        System.out.println(dist);
+
+    }
+
+}
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        Point point = new Point(0, 0);
+        Point point1 = new Point(2, 0);
+        double out = point.distance(point1);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when20to20then0() {
+        double expected = 0;
+        Point point = new Point(2, 0);
+        Point point1 = new Point(2, 0);
+        double out = point.distance(point1);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when22to20then2() {
+        double expected = 2;
+        Point point = new Point(2, 2);
+        Point point1 = new Point(2, 0);
+        double out = point.distance(point1);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when22to23then1() {
+        double expected = 1;
+        Point point = new Point(2, 2);
+        Point point1 = new Point(2, 3);
+        double out = point.distance(point1);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
