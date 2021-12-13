0958d7524c8cddc07202cbb308e49a7050869399
c0het <c0het401@gmail.com>
2021-12-13 19:23:32 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,31 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] item = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < item.length; i++) {
+            if (items[i] != null) {
+                item[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(item, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = findAll();
+        int size = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            String name = rsl[i].getName();
+            if (key.equals(name)) {
+                rsl[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
     }
 
     public Item findById(int id) {
