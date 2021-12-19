a915e81bf151d1bb043bb94a6ffa2eac36063dfc
c0het <c0het401@gmail.com>
2021-12-19 11:40:47 +0300

    Реализация класса StartUI. Вывод заявок по имени

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StartUI {
@@ -57,6 +58,18 @@ public class StartUI {
                     System.out.println(item);
                 } else {
                     System.out.println("Заявка с веденным id: " + id + "не найдена");
+                }
+            } else if (select == 5) {
+                System.out.println("=== Find items by name ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем:" + name + " не найдены.");
                 }
             } else if (select == 6) {
                 run =  false;
