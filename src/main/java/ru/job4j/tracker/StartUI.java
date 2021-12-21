c5f570afcf4fb621e8ee17d22fffe73b925d1164
c0het <c0het401@gmail.com>
2021-12-21 15:12:55 +0300

    Input и полиморфизм

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI {
 
-    public static void creatItem(Input input, Tracker tracker) {
+    public static void createItem(Input input, Tracker tracker) {
         System.out.println("=== Create a new Item===");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
@@ -74,7 +74,7 @@ public class StartUI {
             showMenu();
             int select = input.askInt("Select: ");
             if (select == 0) {
-               StartUI.creatItem(input, tracker);
+               StartUI.createItem(input, tracker);
             } else if (select == 1) {
                 StartUI.showAllItems(tracker);
             } else if (select == 2) {
