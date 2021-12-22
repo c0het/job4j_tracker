56e4d3c578c03386d16762f3fc5f05894456ac56
c0het <c0het401@gmail.com>
2021-12-22 19:18:53 +0300

    Реализация меню за счет шаблона стратегия.

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -9,8 +9,4 @@ public class Square implements Shape {
                 + "|    |" + ln
                 + " ¯¯¯¯";
     }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
-    }
 }
