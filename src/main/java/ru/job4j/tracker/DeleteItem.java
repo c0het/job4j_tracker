6e6a19b950186230638c5e3cda20c1680a994d24
c0het <c0het401@gmail.com>
2021-12-22 19:17:20 +0300

    Реализация меню за счет шаблона стратегия.

---
+++ src/main/java/ru/job4j/tracker/DeleteItem.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class DeleteItem implements UserAction {
+    public String name() {
+        return "Delete item";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Delete item ===");
+        int id = input.askInt("Enter id: ");
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
+        } else {
+            System.out.println("Ошибка удаления заявка.");
+        }
+        return true;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/FindItemById.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class FindItemById implements UserAction {
+    public String name() {
+        return "Find item by id";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Find item by id ===");
+        int id = input.askInt("Enter id: ");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("Заявка с веденным id: " + id + "не найдена");
+        }
+        return true;
+    }
+}
