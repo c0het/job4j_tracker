15247a34b159939052e061356c27c149b52ff372
c0het <c0het401@gmail.com>
2021-12-25 12:27:18 +0300

    Зависимость от System.out

--- src/main/java/ru/job4j/tracker/CloseAction.java
+++ src/main/java/ru/job4j/tracker/CloseAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class CloseAction implements UserAction {
+    private final Output output;
+
+    public CloseAction(Output output) {
+        this.output = output;
+    }
+
     @Override
     public String name() {
         return "Exit program";
