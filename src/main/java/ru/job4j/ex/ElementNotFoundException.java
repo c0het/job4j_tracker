fd856c5ca54bac3f57ee150906bb6323f504fd1a
c0het <c0het401@gmail.com>
2021-12-26 19:41:16 +0300

    Пользовательские исключения.

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,26 +1,7 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                rsl = i;
-                break;
-            } else {
-                throw new ElementNotFoundException();
-            }
-        }
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        String[] value = {"car", "dog", "cat"};
-        String key = "bus";
-        try {
-            indexOf(value, key);
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
+    public ElementNotFoundException(String messege) {
+        super(messege);
     }
 }
