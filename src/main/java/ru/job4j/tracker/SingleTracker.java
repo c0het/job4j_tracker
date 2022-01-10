13a2b455b86606bf80ac6fb04c8afcc325549914
c0het <c0het401@gmail.com>
2022-01-10 20:00:54 +0300

    Singleton

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private static Tracker tracker = null;
+
+    private SingleTracker() {
+
+    }
+
+    public static Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
