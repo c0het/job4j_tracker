31845a0e8ff27391bd13bc3a8112c8b1c119ade2
c0het <c0het401@gmail.com>
2022-02-21 17:31:47 +0300

    4. Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -8,18 +8,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int numberLeft = 0;
-        int numberRight = 0;
-        for (int i = 0; i < Math.min(left.split("\\. ").length, right.split("\\. ").length); i++) {
-            try {
-                numberLeft = Integer.parseInt(left.split("\\. ")[i]);
-                numberRight = Integer.parseInt(right.split("\\. ")[i]);
-               break;
-            } catch (NumberFormatException e) {
-                System.out.println("В строке нету чисел");
-            }
-
-        }
+        int numberLeft = Integer.parseInt(left.split("\\. ")[0]);
+        int numberRight = Integer.parseInt(right.split("\\. ")[0]);
         return Integer.compare(numberLeft, numberRight);
     }
 }
