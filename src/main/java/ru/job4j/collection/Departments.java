10a98f8d01025722db3272d6e3dc4365b58cbb83
c0het <c0het401@gmail.com>
2022-03-23 17:36:21 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,7 +9,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start == "") {
+                if (start.equals("")) {
                 setRsl.add(el);
                 start = el;
                 } else {
